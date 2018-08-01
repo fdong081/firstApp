fee2b218eb1a192fa45e222f1aaadbf4b3ecc5e5
Dong fenfang <dongfenfang@dongs-macbook-pro.local>
2018-08-01 15:51:21 -0400

    finished

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelroll: UILabel!
+    
+    @IBOutlet weak var upImageView: UIImageView!
+    
+    @IBOutlet weak var downImageView: UIImageView!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let num1 = arc4random_uniform(6) + 1
+        let num2 = arc4random_uniform(6) + 1
+        
+        labelroll.text = "The sum is: \(num1 + num2)"
+        
+        upImageView.image = UIImage(named: "Dice\(num1)")
+        
+        downImageView.image = UIImage(named: "Dice\(num2)")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
